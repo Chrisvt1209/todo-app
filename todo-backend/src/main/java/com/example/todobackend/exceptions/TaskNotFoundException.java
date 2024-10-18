cea71de1971fb8153ad3de52cbdbca09b91a3329
Chris van Tiggelen <chrisvt@gitkraken.nl>
2024-10-18 22:28:22 +0200

    Update TaskNotFoundException to generic NotFoundException

--- todo-backend/src/main/java/com/example/todobackend/exceptions/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.todobackend.exceptions;
-
-public class TaskNotFoundException extends RuntimeException {
-  public TaskNotFoundException(String message) {
-    super(message);
-  }
-}
